3a6116db8797c622ec8d8c1b259b4ae10517c4e3
divyagoyal3 <goyaldivya631@gmail.com>
2025-04-05 19:11:20 +0530

    day6

---
+++ src/components/Card.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+// import image from "../assets/image.avif";
+import { LuLeafyGreen } from "react-icons/lu";
+import { GiChickenOven } from "react-icons/gi";
+
+function Card({name,image,id,price,type}){
+    return (
+        <div className='w-[300px] h-[400px] bg-white p-3 rounded-lg flex flex-col gap-3 shadow-lg hover:border-2
+         border-green-300'>
+            <div className='w-[100%] h-[60%] overflow-hidden rounded-lg'>
+                <img src={image} alt="" className='object-cover ' />
+            </div>
+            <div className='font-semibold text-2xl'>
+               {name}
+            </div>
+            <div className='w-full flex justify-between items-center'>
+                <div className='text-lg font-bold text-green-500'>Rs {price}/-</div>
+                <div className='flex justify-between items-center gap-2 text-green-500 text-lg font-bold'>
+                   {type ==="veg"?<LuLeafyGreen />:<GiChickenOven/>} <span>{type}</span></div>
+            </div>
+            <div className='w-full h-[40px] bg-green-350 rounded-md flex justify-center items-center text-white font-semibold cursor-pointer hover:bg-green-600 transition-all duration-200'>
+                Add to Cart
+            </div>
+        </div>
+    
+    )
+}
+ 
+
+export default Card
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,27 @@
 import React from 'react'
 import Nav from '../components/Nav'
 import Categories from '../Category'
+import Card from '../components/Card'
+import {food_items} from '../food'
 
 const Home = () => {
   return (
-    <div className='bg-slate-200  w-full min-h-screen'>
+    <div className='bg-slate-200  w-full h-[3200px] '>
       <Nav />
       
-       <div>
+       <div className='flex flex-wrap justify-center items-center gap-5 w-[100%] '>
         {Categories.map((item) =>{
-            return <div>
+            return <div className='w-[100px] h-[100px] bg-white rounded-lg shadow-lg flex flex-col text-[15px]
+             text-gray-500 font-semibold justify-center items-center gap-2 hover:bg-green-200 cursor-pointer transition-all duration-200'>
+                 {item.image}
                 {item.name}
-                {item.image}
+               
             </div>
         })}
+      </div>
+      <div className='w-full h-[100px] flex flex-wrap gap-5 pt-8 pb-8 justify-center items-center'>
+        {food_items.map((item)=>(<Card name ={item.food_name}  
+        image={item.food_image} price={item.price} id={item.id} type={item.food_type}/>))}
       </div>
       </div>
     
